183d249d645d0c3caf981a8ef72b69b1c03e830e
Oliver <oliver@archer.fi>
2020-05-09 15:23:42 +0100

    Edited file to make it work with new game struct methods

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,20 @@
-mod render_game;
+mod game;
 mod structs;
-mod new_cells_vec;
-mod quick_sort;
-mod dice;
 
-use std::thread;
-
-use structs::{Cell, Game};
-use render_game::render;
-use new_cells_vec::make_cells_vec;
-use quick_sort::quick_sort;
-use std::thread::JoinHandle;
+use std::{ thread, thread::JoinHandle };
+use game::Game;
 
 fn main() {
   // Initialises the game by creating a game variable using the game struct
-  let game = Game {
-    active: false,
-    player_count: None,
-    players: None
-  };
-
-  // Uses the make cells vec function to make a vec of cells then sorts the cells into order using a quick sort
-  let mut cells: Vec<Cell> = make_cells_vec();
-  let vec_len: i64 = (cells.len()-1) as i64;
-  quick_sort(&mut cells, 0, vec_len);
+  let mut game = Game::new();
+  game.quick_sort(0, (game.cells.len()-1) as i64);
 
   // Creates new vec to store opened threads
   let mut open_threads: Vec<JoinHandle<()>> = Vec::new();
 
   // Uses the render board module to render the game board
   open_threads.push(thread::spawn(move|| {
-    render(game, cells);
+    game.render()
   }));
   
   #[allow(unused_must_use)]
