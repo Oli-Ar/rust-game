b32cf84b4d65d75e4faccbbb8d042a188c2f7471
Oliver <oliver@archer.fi>
2020-05-09 19:35:48 +0100

    Edited comments to match changes

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -1,10 +1,11 @@
 use crate::structs::Cell;
 
 // Static function owned by Game struct used to create a vector of cells
+// Here x is the length and width of the game board
 pub fn make_cells_vec(x: i32) -> Vec<Cell> {
-  let mut cells_vec: Vec<Cell> = Vec::with_capacity((x*x) as usize); // Makes vec with a capacity of 19
+  let mut cells_vec: Vec<Cell> = Vec::with_capacity((x*x) as usize); // Makes vec with a capacity of x^2
   for i in 0..(x*x) {
-    // A number between 1 and 49 that matches the order of the game (https://prnt.sc/sbeizh)
+    // A number between 1 and x^2 that matches the order of the game
     let cell_num: i32;
     if x%2 == 1 {
       if i/x%2 == 1 { cell_num = x*(x-i/x)-(i%x) } else { cell_num = x*((x-1)-i/x)+(i%x)+1 };
@@ -12,9 +13,9 @@ pub fn make_cells_vec(x: i32) -> Vec<Cell> {
       if i/x%2 == 1 { cell_num = x*((x-1)-i/x)+(i%x)+1 } else { cell_num = x*(x-i/x)-(i%x) };
     };
     let new_cell: Cell = Cell {
-      cell_number: cell_num,  
-      x: i % x, // Value between 0 and 6 which will be used to plot the cell on the x axis
-      y: i / x, // Value between 0 and 6 which will be used to plot the cell on the y axis
+      cell_number: cell_num,
+      x: i % x, // Value between 0 and x which will be used to plot the cell on the x axis
+      y: i / x, // Value between 0 and x which will be used to plot the cell on the y axis
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
