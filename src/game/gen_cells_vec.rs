6c40147b88a0cfe601c7c91297736d88a94953db
Oliver <oliver@archer.fi>
2020-05-09 18:42:51 +0100

    replaced all instances of 7 with x representing board size, and added checks to ensure even with even cell one is bottom left

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -1,16 +1,20 @@
 use crate::structs::Cell;
 
 // Static function owned by Game struct used to create a vector of cells
-pub fn make_cells_vec() -> Vec<Cell> {
-  let mut cells_vec: Vec<Cell> = Vec::with_capacity(49); // Makes vec with a capacity of 19
-  for i in 0..49 {
+pub fn make_cells_vec(x: i32) -> Vec<Cell> {
+  let mut cells_vec: Vec<Cell> = Vec::with_capacity((x*x) as usize); // Makes vec with a capacity of 19
+  for i in 0..(x*x) {
     // A number between 1 and 49 that matches the order of the game (https://prnt.sc/sbeizh)
     let cell_num: i32;
-    if i/7%2 == 1 { cell_num = 7*(7-i/7)-(i%7) } else { cell_num = 7*(6-i/7)+(i%7)+1 };
+    if x%2 == 1 {
+      if i/x%2 == 1 { cell_num = x*(x-i/x)-(i%x) } else { cell_num = x*((x-1)-i/x)+(i%x)+1 };
+    } else {
+      if i/x%2 == 1 { cell_num = x*((x-1)-i/x)+(i%x)+1 } else { cell_num = x*(x-i/x)-(i%x) };
+    };
     let new_cell: Cell = Cell {
-      cell_number: cell_num,
-      x: i % 7, // Value between 0 and 6 which will be used to plot the cell on the x axis
-      y: i / 7, // Value between 0 and 6 which will be used to plot the cell on the y axis
+      cell_number: cell_num,  
+      x: i % x, // Value between 0 and 6 which will be used to plot the cell on the x axis
+      y: i / x, // Value between 0 and 6 which will be used to plot the cell on the y axis
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
