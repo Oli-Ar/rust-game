d863de7820d10ae64edbe952919678fbdba69d3f
Oliver <oliver@archer.fi>
2020-05-09 15:13:16 +0100

    Ported code to create a vec of cells to a static function used by the game struct, as the struct is the only code to have a vec of cells

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -1,5 +1,6 @@
 use crate::structs::Cell;
 
+// Static function owned by Game struct used to create a vector of cells
 pub fn make_cells_vec() -> Vec<Cell> {
   let mut cells_vec: Vec<Cell> = Vec::with_capacity(49); // Makes vec with a capacity of 19
   for i in 0..49 {
@@ -10,9 +11,8 @@ pub fn make_cells_vec() -> Vec<Cell> {
       cell_number: cell_num,
       x: i % 7, // Value between 0 and 6 which will be used to plot the cell on the x axis
       y: i / 7, // Value between 0 and 6 which will be used to plot the cell on the y axis
-      player: None, // States no player is on the board currently
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
-  return cells_vec; // Returns a vector containing 49 cells
+  return cells_vec;
 }
