8a2504f2396eac95dafff7f2f6eee32340c8c99a
Oliver <oliver@archer.fi>
2020-05-15 11:13:07 +0100

    Adjusted to work with render_turn function
    File is adjusted to correctly render the board with a 50px gap at the top of the window to allow for text displaying which players turn it is.

--- src/game/render/game_board.rs
+++ src/game/render/game_board.rs
@@ -1,6 +1,4 @@
-extern crate piston_window;
-
-use piston_window::{ PistonWindow, Rectangle, Text, Transformed, Glyphs, Size, Event, Context, G2d };
+use piston_window::{ PistonWindow, Rectangle, Text, Transformed, Glyphs, Size, Event, Context, G2d, clear };
 use crate::structs::Cell;
 use crate::game::Game;
 
@@ -13,6 +11,9 @@ pub fn render_board(window: &mut PistonWindow,
                     no_tex: Text,
 ) {
   window.draw_2d(e, |c, g, device| {
+    // Sets background to white
+    clear([1.0, 1.0, 1.0, 1.0], g);
+
     // Draw each cell in cells as a grid
     for i in &game.cells {
       // Separate functions to render different sections of board
@@ -23,23 +24,25 @@ pub fn render_board(window: &mut PistonWindow,
   });
 }
 
-fn draw_cells(game: &Game, rect: Rectangle, window_size: Size, i: &Cell, c: Context, g: &mut G2d) {
+fn draw_cells(game: &Game, rect: Rectangle, window_size: Size, i: &Cell, c: Context, g: &mut G2d<'_>) {
   // Defines the size of the rectangle as 1/7th the size of the window to allow 7*7 grid
   let rect_size = [
     i.x as f64*(window_size.width / game.board_size as f64), // Location on x axis of current cell
-    i.y as f64*(window_size.height / game.board_size as f64), // Location on y axis of cell
+    i.y as f64*((window_size.height-50.0) / game.board_size as f64)+50.0, // Location on y axis of cell
     (window_size.width / game.board_size as f64), // Size of cell on x axis
-    (window_size.height / game.board_size as f64)]; // Size of cell on y axis
+    ((window_size.height-50.0) / game.board_size as f64)]; // Size of cell on y axis
 
   rect.draw(rect_size, &c.draw_state, c.transform, g);
 }
 
-fn draw_nums(game: &Game, text: Text, window_size: Size, i: &Cell, glyphs: &mut Glyphs, c: Context, g: &mut G2d) {
+fn draw_nums(game: &Game, text: Text, window_size: Size, i: &Cell, glyphs: &mut Glyphs, c: Context, g: &mut G2d<'_>) {
   // Converts the int of the current square to a string which can be coerced into str
   let sqr_num = i.cell_number.to_string();
   // Defines the transformation for the position of the number
   let transform = c.transform.trans((i.x as f64 + 0.3)*window_size.width/game.board_size as f64,
-                                    window_size.height/game.board_size as f64/1.5+(i.y as f64+0.1)*window_size.height/game.board_size as f64);
+                                    50.0+window_size.height/game.board_size as f64/1.5 +
+                                    (i.y as f64+0.1)*(window_size.height-50.0)/game.board_size as f64
+  );
 
   // Renders and displays the number of the square
   text.draw(&sqr_num, glyphs, &c.draw_state, transform, g).unwrap();
