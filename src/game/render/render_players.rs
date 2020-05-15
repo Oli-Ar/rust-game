1682683ffeb22864e5c47f4c1d4c020360b60667
Oliver <oliver@archer.fi>
2020-05-15 11:16:21 +0100

    Updated to render correctly with render_turn function
    Player rendering now offset by 50px on the y axis to account for the player turn text at the top of the window.

--- src/game/render/render_players.rs
+++ src/game/render/render_players.rs
@@ -1,5 +1,3 @@
-extern crate piston_window;
-
 use piston_window::{ PistonWindow, Ellipse, ellipse, Size, Event, Context, G2d };
 use crate::game::Game;
 
@@ -25,7 +23,7 @@ pub fn render_players(game: &mut Game, window: &mut PistonWindow, e: &Event, win
         // Using data from the cell the player is on the size and position of counter is defined
         let counter_size = (window_size.width+window_size.height)/2.0/(game.board_size*6) as f64;
         let cell_x = cell.x as f64*(window_size.width / game.board_size as f64);
-        let cell_y = cell.y as f64*(window_size.height / game.board_size as f64);
+        let cell_y = cell.y as f64*((window_size.height-50.0) / game.board_size as f64)+50.0;
 
         // Match statement to check player number and then draw the counter in a certain colour
         // Player 1 is drawn top left, 2: top right, 3: bottom left, 4: bottom right
@@ -41,7 +39,7 @@ pub fn render_players(game: &mut Game, window: &mut PistonWindow, e: &Event, win
           2 => {
             draw_circle([0.0, 1.0, 1.0, 1.0],
                         counter_size,
-                        cell_x+window_size.width/7.0-counter_size-5.0,
+                        cell_x+window_size.width/game.board_size as f64-counter_size-5.0,
                         cell_y+5.0,
                         c, &mut g
             );
@@ -50,15 +48,15 @@ pub fn render_players(game: &mut Game, window: &mut PistonWindow, e: &Event, win
             draw_circle([0.0, 0.0, 1.0, 1.0],
                         counter_size,
                         cell_x+5.0,
-                        cell_y+window_size.height/7.0-counter_size-5.0,
+                        cell_y+(window_size.height-50.0)/game.board_size as f64-counter_size-5.0,
                         c, &mut g
             );
           },
           4 => {
             draw_circle([1.0, 1.0, 0.0, 1.0],
                         counter_size,
-                        cell_x+window_size.width/7.0-counter_size-5.0,
-                        cell_y+window_size.height/7.0-counter_size-5.0,
+                        cell_x+window_size.width/game.board_size as f64-counter_size-5.0,
+                        cell_y+(window_size.height-50.0)/game.board_size as f64-counter_size-5.0,
                         c, &mut g
             );
           },
@@ -70,7 +68,7 @@ pub fn render_players(game: &mut Game, window: &mut PistonWindow, e: &Event, win
 }
 
 // Function to draw a circle representing a player
-fn draw_circle(color: [f32; 4], size: f64, x_pos: f64, y_pos: f64, c: Context, g: &mut G2d) {
+fn draw_circle(color: [f32; 4], size: f64, x_pos: f64, y_pos: f64, c: Context, g: &mut G2d<'_>) {
   let circle = Ellipse { color, ..COUNTER };
 
   let counter_pos: [f64; 4] = [
