db9a104bb7092cf939f270a9e0f1ca288d808dc0
Oliver <oliver@archer.fi>
2020-05-15 11:10:01 +0100

    Updated file to check player turns and to display winner
    File now has turn variable which is updated by the render_turn function, the variable is an i32 between 1 and the amount of players.

--- src/game/render.rs
+++ src/game/render.rs
@@ -1,16 +1,17 @@
-extern crate piston_window;
-extern crate find_folder;
-
-use piston_window::{ PistonWindow, WindowSettings, Window, Rectangle, rectangle, Text, };
+use piston_window::{ PistonWindow, WindowSettings, Window, Rectangle, rectangle, Text };
 use crate::game::Game;
 
 mod game_board;
 mod player_amnt_input;
 mod render_players;
+mod render_turn;
+mod render_winner;
 
 use game_board::render_board;
 use player_amnt_input::get_input;
 use render_players::render_players;
+use render_turn::render_turn;
+use render_winner::render_winner;
 
 const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
 const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
@@ -27,21 +28,27 @@ impl Game {
     let mut glyphs = window.load_font(assets.join("FiraSans-Regular.ttf")).unwrap();
 
     // Defines the base shapes and text to be rendered
-    let border = rectangle::Border{ color: BLACK, radius: 1.0 };
-    let rect = Rectangle{ color: WHITE, shape: rectangle::Shape::Square, border: Some(border) };
-    let text_to_render = Text{ color: BLACK, font_size: 40, round: true };
-    let small_text = Text{ font_size: 20, ..text_to_render };
-    let no_txt = Text{ font_size: 220/self.board_size as u32, ..text_to_render };
+    let border = rectangle::Border { color: BLACK, radius: 1.0 };
+    let rect = Rectangle { color: WHITE, shape: rectangle::Shape::Square, border: Some(border) };
+    let text_to_render = Text { color: BLACK, font_size: 40, round: true };
+    let small_text = Text { font_size: 20, ..text_to_render };
+    let num_txt = Text { font_size: 220/self.board_size as u32, ..text_to_render };
 
     // While events can take place perform actions to window
+    let mut turn: i32 = 1;
     while let Some(e) = window.next() {
       // Gets the size of the window to be used in resizing the game board
       let window_size = window.size();
+
+      // Checks if game is running
       if self.active == false {
         get_input(&mut window, e, window_size, self, text_to_render, small_text, &mut glyphs);
-      } else {
-        render_board(&mut window, &e, self, window_size, &mut glyphs, rect, no_txt);
+      } else if self.active == true && self.top_player.as_ref().unwrap().score < self.board_size*self.board_size {
+        render_board(&mut window, &e, self, window_size, &mut glyphs, rect, num_txt);
         render_players(self, &mut window, &e, window_size);
+        turn = render_turn(self, &mut window, window_size, &mut glyphs, &e, small_text, turn);
+      } else {
+        render_winner(&mut window, e, window_size, self, text_to_render, &mut glyphs);
       }
     };
   }
