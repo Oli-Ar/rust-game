d01e0311848994059632b92b67d7448a42014dcf
Oliver <oliver@archer.fi>
2020-05-17 20:22:51 +0100

    Removed redundant code

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,10 @@
 mod game;
 mod structs;
 
-use std::{ thread, thread::JoinHandle };
 use game::Game;
 
 fn main() {
   // Initialises the game by creating a game variable using the game struct
   let mut game = Game::new(7);
-  game.quick_sort(0, (game.cells.len()-1) as i64);
-
-  // Creates new vec to store opened threads
-  let mut open_threads: Vec<JoinHandle<()>> = Vec::new();
-
-  // Uses the render board module to render the game board
-  open_threads.push(thread::spawn(move|| {
-    game.render()
-  }));
-  
-  #[allow(unused_must_use)]
-  // Closes open threads once they have ran
-  for thread in open_threads {
-    thread.join();
-  };
+  game.render()
 }
