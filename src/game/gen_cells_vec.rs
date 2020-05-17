5cfef9589a3e6884a3c708cda2a74cfab2a4d6a7
Oliver <oliver@archer.fi>
2020-05-17 20:25:20 +0100

    File now generates obstacles for players to interract with

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -19,17 +19,44 @@ pub fn make_cells_vec(x: i32) -> Vec<Cell> {
       cell_number: cell_num,
       x: i % x, // Value between 0 and x which will be used to plot the cell on the x axis
       y: i / x, // Value between 0 and x which will be used to plot the cell on the y axis
-      // start: false,
-      // end: None,
+      start: false,
+      end: None,
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
+  // Sorts cells by their cell number
   quick_sort(&mut cells_vec, 0, (x*x-1) as i64);
-  // let obstacles = gen_obstacles(x*x);
-  // for i in obstacles {
-  //   cells_vec[i[0] as usize].start = true;
-  //   cells_vec[i[0] as usize].end = Some(&cells_vec[i[1] as usize]);
-  // };
-  println!("{:?}", cells_vec);
+  // Generates a vector of obstacles and assigns them to the correct cells
+  let obstacles = gen_obstacles(x);
+  for i in obstacles {
+    cells_vec[i[0] as usize].start = true;
+    cells_vec[i[0] as usize].end = Some(i[0] + i[1]);
+  };
   return cells_vec;
+}
+
+fn gen_obstacles(x: i32) -> Vec<Vec<i32>> {
+  let mut rng = thread_rng();
+  // Generates a normal for generating arrows with a standard deviation of 15
+  let normal = Normal::new(0.0, 15.0).unwrap();
+  let mut obs_vec: Vec<Vec<i32>> = Vec::with_capacity((x*x) as usize);
+  for _i in 0..(x*2) {
+    // Do-while loop that generates an arrow for a random cell
+    // Exit condition is: the arrow being longer than 0, not pointing to the same cell as another arrow,
+    // arrow not pointing off the board, and not pointing to the end of a previous arrow, however
+    // another arrow can start at the end of another arrow - the check is in place to prevent arrows
+    // pointing at each other and leaving the player in an infinite loop leading to stack overflow
+    loop {
+      let cell: i32 = rng.gen_range(9, x*x);
+      let v: i32 = ((normal.sample(&mut thread_rng()) as f64).round()) as i32;
+      if (cell + v > 0) && (cell + v < x*x-1) && (v != 0) && (cell != x*x)
+      && (obs_vec.iter().all(|i| {
+        i[0]+i[1] != cell+v && i[0] != cell+v
+      })) {
+        obs_vec.push(vec![cell, v]);
+        break;
+      };
+    };
+  };
+  return obs_vec;
 }
