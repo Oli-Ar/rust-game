e818552fdd5a753947cace54ee6c464b76b10cbb
Oliver <oliver@archer.fi>
2020-05-17 20:31:01 +0100

    Obstacles are now rendered on the board

--- src/game/render/game_board.rs
+++ src/game/render/game_board.rs
@@ -1,4 +1,4 @@
-use piston_window::{ PistonWindow, Rectangle, Text, Transformed, Glyphs, Size, Event, Context, G2d, clear };
+use piston_window::{ PistonWindow, Rectangle, Text, Transformed, Glyphs, Size, Event, Context, G2d, clear, Line };
 use crate::structs::Cell;
 use crate::game::Game;
 
@@ -18,9 +18,12 @@ pub fn render_board(window: &mut PistonWindow,
     // Draw each cell in cells as a grid
     for i in &game.cells {
       // Separate functions to render different sections of board
-      draw_cells(game, rect, window_size, i, c, g);
-      draw_nums(game, text, window_size, i, glyphs, c, g);
+      draw_cells(game, rect, window_size, &i, c, g);
+      draw_nums(game, text, window_size, &i, glyphs, c, g);
     };
+    for i in &game.cells {
+      if i.start == true { draw_line(&game, i, window_size, c, g)}
+    }
     glyphs.factory.encoder.flush(device);
   });
 }
@@ -40,11 +43,41 @@ fn draw_nums(game: &Game, text: Text, window_size: Size, i: &Cell, glyphs: &mut 
   // Converts the int of the current square to a string which can be coerced into str
   let sqr_num = i.cell_number.to_string();
   // Defines the transformation for the position of the number
-  let transform = c.transform.trans((i.x as f64 + 0.3)*window_size.width/game.board_size as f64,
-                                    50.0+window_size.height/game.board_size as f64/1.5 +
-                                    (i.y as f64+0.1)*(window_size.height-50.0)/game.board_size as f64
+  let transform = c.transform.trans(
+    (i.x as f64 + 0.3)*window_size.width/game.board_size as f64,
+    50.0+window_size.height/game.board_size as f64/1.5 +
+        (i.y as f64+0.1)*(window_size.height-50.0)/game.board_size as f64
   );
 
   // Renders and displays the number of the square
   text.draw(&sqr_num, glyphs, &c.draw_state, transform, g).unwrap();
+}
+
+fn draw_line(game: &Game, cell: &Cell, window_size: Size, c: Context, g: &mut G2d<'_>) {
+  // Sets a different colour and offset depending on if the line is a snake or a ladder
+  let color: [f32; 4];
+  let offset: f64;
+  if cell.cell_number > game.cells[cell.end.unwrap() as usize].cell_number {
+    color = [1.0, 0.0, 0.0, 1.0];
+    offset = -10.0;
+  } else {
+    color = [0.0, 1.0, 0.0, 1.0];
+    offset = 10.0;
+  }
+  let line = Line {
+    color,
+    radius: 2.0,
+    shape: piston_window::line::Shape::Round
+  };
+  // Defines the coordinates of the arrow as [hx, hy, tx, ty] where h is head and t is tail
+  // the arrows are offset to not overlap severely with each other
+  let arrow = [
+    (cell.x as f64 + 0.5)*window_size.width/game.board_size as f64 + offset,
+    50.0+window_size.height/game.board_size as f64/2.0 +
+      (cell.y as f64)*(window_size.height-50.0)/game.board_size as f64 + offset,
+    (game.cells[cell.end.unwrap() as usize].x as f64 + 0.5)*window_size.width/game.board_size as f64 + offset,
+    50.0+window_size.height/game.board_size as f64/2.0 +
+        (game.cells[cell.end.unwrap() as usize].y as f64)*(window_size.height-50.0)/game.board_size as f64 + offset,
+  ];
+  line.draw_arrow(arrow, 5.0, &c.draw_state, c.transform, g)
 }
