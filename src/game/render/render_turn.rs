3f2430b40c6356e536989a6e59778d054e4e41fb
Oliver <oliver@archer.fi>
2020-05-17 20:26:32 +0100

    Player interractions with obstacles are now rendered

--- src/game/render/render_turn.rs
+++ src/game/render/render_turn.rs
@@ -1,7 +1,7 @@
 use piston_window::{ PistonWindow, Event, Text, Size, Glyphs, Transformed, ButtonState, ButtonEvent };
 use crate::game::Game;
 
-pub fn render_turn(game: &mut Game,
+pub fn render_turn(game: & mut Game,
                    window: &mut PistonWindow,
                    window_size: Size,
                    glyphs: &mut Glyphs,
@@ -11,7 +11,7 @@ pub fn render_turn(game: &mut Game,
 ) -> i32 {
   let mut new_turn = turn;
 
-  // Check for keypress event
+  // Check for keypress event - returns an i32 representing which players turn it is
   if let Some(k) = e.button_args() {
     if k.state == ButtonState::Press {
       let res = game.turn_keypress(&k.button, turn);
@@ -26,6 +26,7 @@ pub fn render_turn(game: &mut Game,
     let top_transform = c.transform.trans(window_size.width/2.0-227.0, 33.0);
     text.draw(top_text, glyphs, &c.draw_state, top_transform, g).unwrap();
 
+    // Checks the previous players roll if it is some and displays it at the bottom of the window
     let prev_player = if turn == 1 { game.player_count.unwrap()-1 } else { turn-2 };
     let player = &game.players.as_ref().unwrap()[(prev_player) as usize];
     if player.roll.is_some() {
@@ -38,10 +39,24 @@ pub fn render_turn(game: &mut Game,
         roll_data.roll_total,
         roll_data.old_score, roll_data.new_score
       );
-      let bottom_transform = c.transform.trans(window_size.width/2.0-234.0, window_size.height+30.0);
+      let bottom_transform = c.transform.trans(window_size.width/2.0-234.0, window_size.height+13.0);
       text.draw(bottom_text, glyphs, &c.draw_state, bottom_transform, g).unwrap();
-    }
 
+      // Checks if on the previous roll the player landed on an obstacle
+      if roll_data.obstacles.is_some() {
+        let obstacles = roll_data.obstacles.clone().unwrap();
+        // For each of the obstacles the player landed on the type, position of the obstacle, and
+        // where it took the player is rendered to the bottom of the screen
+        for (i, o) in obstacles.iter().enumerate() {
+          let obstacle_text = &format!(
+            "Square {} was the start of a {} moving player {} to square {}",
+            o.start, o.obstacle.to_string().to_ascii_lowercase(), player.number, o.end
+          );
+          let obstacle_transform = c.transform.trans(window_size.width/2.0-215.0, window_size.height+((2.0+i as f64)*13.0));
+          text.draw(obstacle_text, glyphs, &c.draw_state, obstacle_transform, g).unwrap();
+        }
+      }
+    }
   });
   return new_turn;
 }
