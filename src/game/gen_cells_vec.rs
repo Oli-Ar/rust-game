210a64c49cc5a280bbda2a7ec97841ff1ed42c9e
Oliver <oliver@archer.fi>
2020-05-17 20:19:47 +0100

    rand_distr used as new dependency

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -1,4 +1,7 @@
 use crate::structs::Cell;
+use rand_distr::{ Normal, Distribution };
+use rand::{ thread_rng, Rng };
+use crate::game::quick_sort_cells::quick_sort;
 
 // Static function owned by Game struct used to create a vector of cells
 // Here x is the length and width of the game board
@@ -16,8 +19,17 @@ pub fn make_cells_vec(x: i32) -> Vec<Cell> {
       cell_number: cell_num,
       x: i % x, // Value between 0 and x which will be used to plot the cell on the x axis
       y: i / x, // Value between 0 and x which will be used to plot the cell on the y axis
+      // start: false,
+      // end: None,
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
+  quick_sort(&mut cells_vec, 0, (x*x-1) as i64);
+  // let obstacles = gen_obstacles(x*x);
+  // for i in obstacles {
+  //   cells_vec[i[0] as usize].start = true;
+  //   cells_vec[i[0] as usize].end = Some(&cells_vec[i[1] as usize]);
+  // };
+  println!("{:?}", cells_vec);
   return cells_vec;
 }
