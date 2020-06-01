306c36e72fadf42e8843d9ac6b1aaedc97dc59c1
Oliver <oliver@archer.fi>
2020-06-01 10:23:32 +0100

    Cells are now generated in an order such that they don't need to be sorted hence removal of quicksort function

--- src/game/gen_cells_vec.rs
+++ src/game/gen_cells_vec.rs
@@ -1,31 +1,33 @@
 use crate::structs::Cell;
 use rand_distr::{ Normal, Distribution };
 use rand::{ thread_rng, Rng };
-use crate::game::quick_sort_cells::quick_sort;
 
 // Static function owned by Game struct used to create a vector of cells
 // Here x is the length and width of the game board
 pub fn make_cells_vec(x: i32) -> Vec<Cell> {
-  let mut cells_vec: Vec<Cell> = Vec::with_capacity((x*x) as usize); // Makes vec with a capacity of x^2
+  // Makes vec with a capacity of x^2
+  let mut cells_vec: Vec<Cell> = Vec::with_capacity((x*x) as usize);
   for i in 0..(x*x) {
-    // A number between 1 and x^2 that matches the order of the game
-    let cell_num: i32;
-    if x%2 == 1 {
-      if i/x%2 == 1 { cell_num = x*(x-i/x)-(i%x) } else { cell_num = x*((x-1)-i/x)+(i%x)+1 };
+    // Finds the x value of the cell so that it matches the order of the game so the counters
+    // can move in a logical way
+    let x_val: i32;
+    if ( ((x-1)-i/x)%2 == 1 && x%2 == 1 ) || ( ((x-1)-i/x)%2 == 0 && x%2 == 0 ) {
+      x_val = (x-1)-i%x
     } else {
-      if i/x%2 == 1 { cell_num = x*((x-1)-i/x)+(i%x)+1 } else { cell_num = x*(x-i/x)-(i%x) };
+      x_val = i%x
     };
+
     let new_cell: Cell = Cell {
-      cell_number: cell_num,
-      x: i % x, // Value between 0 and x which will be used to plot the cell on the x axis
-      y: i / x, // Value between 0 and x which will be used to plot the cell on the y axis
+      cell_number: i+1, // The number of the cell
+      x: x_val, // Value between 0 and x which will be used to plot the cell on the x axis
+      y: x-1-i/x, // Value between 0 and x which will be used to plot the cell on the y axis
       start: false,
       end: None,
     };
     cells_vec.push(new_cell); // Push cell to the vector of cells
   }
   // Sorts cells by their cell number
-  quick_sort(&mut cells_vec, 0, (x*x-1) as i64);
+  // quick_sort(&mut cells_vec, 0, (x*x-1) as i64);
   // Generates a vector of obstacles and assigns them to the correct cells
   let obstacles = gen_obstacles(x);
   for i in obstacles {
