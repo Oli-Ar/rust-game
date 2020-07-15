127414ec6888d6de78f7cf468a0f6ab60754a3bd
Oliver <oliver@archer.fi>
2020-07-15 14:23:23 +0100

    Arrows now scale with window size, and small edits to colours

--- src/game/render/render_board.rs
+++ src/game/render/render_board.rs
@@ -12,8 +12,8 @@ pub fn render_board(game: &Game,
 ) {
   window.draw_2d(e, |c, g, device| {
     // Sets background to white
-    clear([1.0, 1.0, 1.0, 1.0], g);
-    text.font_size = 220/game.board_size as u32;
+    clear([0.47, 0.75, 0.87, 1.0], g);
+    text.font_size = 220/game.game_options.board_size as u32;
 
     // Draw each cell in cells as a grid
     for i in &game.cells {
@@ -28,13 +28,15 @@ pub fn render_board(game: &Game,
   });
 }
 
-fn draw_cells(game: &Game, rect: Rectangle, window_size: Size, i: &Cell, c: Context, g: &mut G2d<'_>) {
+fn draw_cells(game: &Game, mut rect: Rectangle, window_size: Size, i: &Cell, c: Context, g: &mut G2d<'_>) {
   // Defines the size of the rectangle as 1/7th the size of the window to allow 7*7 grid
   let rect_size = [
-    i.x as f64*(window_size.width / game.board_size as f64), // Location on x axis of current cell
-    i.y as f64*((window_size.height-50.0) / game.board_size as f64)+50.0, // Location on y axis of cell
-    (window_size.width / game.board_size as f64), // Size of cell on x axis
-    ((window_size.height-50.0) / game.board_size as f64)]; // Size of cell on y axis
+    i.x as f64*(window_size.width / game.game_options.board_size as f64), // Location on x axis of current cell
+    i.y as f64*((window_size.height-50.0) / game.game_options.board_size as f64)+50.0, // Location on y axis of cell
+    (window_size.width / game.game_options.board_size as f64), // Size of cell on x axis
+    ((window_size.height-50.0) / game.game_options.board_size as f64)]; // Size of cell on y axis
+  // Checks if cell is even and changes colour to pastel green from default pastel yellow if true
+  if i.cell_number % 2 == 0 { rect.color = [0.47, 0.87, 0.47, 1.0] }
 
   rect.draw(rect_size, &c.draw_state, c.transform, g);
 }
@@ -44,9 +46,9 @@ fn draw_nums(game: &Game, text: Text, window_size: Size, i: &Cell, glyphs: &mut 
   let sqr_num = i.cell_number.to_string();
   // Defines the transformation for the position of the number
   let transform = c.transform.trans(
-    (i.x as f64 + 0.3)*window_size.width/game.board_size as f64,
-    50.0+window_size.height/game.board_size as f64/1.5 +
-        (i.y as f64+0.1)*(window_size.height-50.0)/game.board_size as f64
+    (i.x as f64 + 0.3)*window_size.width/game.game_options.board_size as f64,
+    50.0+window_size.height/game.game_options.board_size as f64/1.5 +
+        (i.y as f64+0.1)*(window_size.height-50.0)/game.game_options.board_size as f64
   );
 
   // Renders and displays the number of the square
@@ -58,26 +60,28 @@ fn draw_line(game: &Game, cell: &Cell, window_size: Size, c: Context, g: &mut G2
   let color: [f32; 4];
   let offset: f64;
   if cell.cell_number > game.cells[cell.end.unwrap() as usize].cell_number {
-    color = [1.0, 0.0, 0.0, 1.0];
-    offset = -10.0;
+    color = [0.95, 0.27, 0.30, 1.0];
+    offset = -10.0*(7.0_f64/game.game_options.board_size as f64);
   } else {
-    color = [0.0, 1.0, 0.0, 1.0];
-    offset = 10.0;
+    color = [0.0, 0.73, 0.89, 1.0];
+    offset = 10.0*(7.0_f64/game.game_options.board_size as f64);
   }
   let line = Line {
     color,
-    radius: 2.0,
+    // Shrinks arrow to a minimum of 1 pixel radius depending on board size (315844 is 562^2) or the
+    // average of 512 and 612 (default game board size) squared
+    radius: 1.0+(11.0_f64/game.game_options.board_size as f64)*((window_size.width*window_size.height)/315844.0_f64),
     shape: piston_window::line::Shape::Round
   };
   // Defines the coordinates of the arrow as [hx, hy, tx, ty] where h is head and t is tail
   // the arrows are offset to not overlap severely with each other
   let arrow = [
-    (cell.x as f64 + 0.5)*window_size.width/game.board_size as f64 + offset,
-    50.0+window_size.height/game.board_size as f64/2.0 +
-      (cell.y as f64)*(window_size.height-50.0)/game.board_size as f64 + offset,
-    (game.cells[cell.end.unwrap() as usize].x as f64 + 0.5)*window_size.width/game.board_size as f64 + offset,
-    50.0+window_size.height/game.board_size as f64/2.0 +
-        (game.cells[cell.end.unwrap() as usize].y as f64)*(window_size.height-50.0)/game.board_size as f64 + offset,
+    (cell.x as f64 + 0.5)*window_size.width/game.game_options.board_size as f64 + offset,
+    50.0+window_size.height/game.game_options.board_size as f64/2.0 +
+      (cell.y as f64)*(window_size.height-50.0)/game.game_options.board_size as f64 + offset,
+    (game.cells[cell.end.unwrap() as usize].x as f64 + 0.5)*window_size.width/game.game_options.board_size as f64 + offset,
+    50.0+window_size.height/game.game_options.board_size as f64/2.0 +
+        (game.cells[cell.end.unwrap() as usize].y as f64)*(window_size.height-50.0)/game.game_options.board_size as f64 + offset,
   ];
-  line.draw_arrow(arrow, 5.0, &c.draw_state, c.transform, g)
+  line.draw_arrow(arrow, 2.0*line.radius, &c.draw_state, c.transform, g);
 }
